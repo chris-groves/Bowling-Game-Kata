5e26c250f32d83cd381f89dae5d7a863f3031732
Chris Groves <chris_groves87@hotmail.co.uk>
2020-01-30 18:04:50 +0000

    Calculates score if one pin is knocked down

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,4 +6,11 @@ describe Game do
     20.times { game.roll(0) }
     expect(game.score).to eq 0
   end
+
+  it 'returns a score of one when one pin is knocked down' do
+    game = Game.new
+    game.roll(1)
+    19.times { game.roll(0) }
+    expect(game.score).to eq 1
+  end
 end
